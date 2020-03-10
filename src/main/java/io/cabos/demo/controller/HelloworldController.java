5ab845a1f1c6ce84a70c11bb13e843b49d56a903
cabos <yuto.eguma@gmail.com>
2020-03-10 22:04:21 +0900

    first commit

---
+++ src/main/java/io/cabos/demo/controller/HelloworldController.java
@@ -0,0 +1,18 @@
+package io.cabos.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author cabos
+ */
+@RestController
+@RequestMapping("/")
+public class HelloworldController {
+
+    @GetMapping("/index")
+    public String index() {
+        return "{\"message\": \"Hello World\"}";
+    }
+}
